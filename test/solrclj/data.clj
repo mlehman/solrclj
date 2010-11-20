bf037cb6629dd8ad5309183906b5278d2da799e0
Matt Lehman <matt.lehman@gmail.com>
2010-11-19 20:04:59 -0500

    test helpers

---
+++ test/solrclj/data.clj
@@ -0,0 +1,19 @@
+(ns solrclj.data)
+
+(def top-selling-books
+     [{:rank 1 :title "A Tale of Two Cities" :author "Charles Dickens" :language "en" :published 1859}
+      {:rank 2 :title "The Lord of the Rings" :author "J. R. R. Tolkien" :language "en" :published 1954}
+      {:rank 3 :title "The Hobbit" :author "J. R. R. Tolkien" :language "en" :published 1937}
+      {:rank 4 :title "Dream of the Red Chamber" :author "Cao Xueqin" :language "zh" :published 1759}
+      {:rank 5 :title "On the Three Representations" :author "Jiang Zemin" :language "zh" :published 2001}
+      {:rank 6 :title "And Then There Were None"  :author "Agatha Christie" :language "en" :published 1939}
+      {:rank 7 :title "The Lion, the Witch and the Wardrobe" :author "C. S. Lewis" :language "en" :published 1950}
+      {:rank 8 :title "She" :author "H. Rider Haggard" :language "en" :published 1887}
+      {:rank 9 :title "The Little Prince" :author "Antoine de Saint-Exupéry" :language "fr" :published 1943}
+      {:rank 10 :title "The Da Vinci Code" :author "Dan Brown" :language "en" :published 2003}
+      {:rank 11 :title "The Catcher in the Rye" :author "J. D. Salinger" :language "en" :published 1951}
+      {:rank 12 :title "The Alchemist" :author "Paulo Coelho" :language "pt" :published 1988}
+      {:rank 13 :title "Steps to Christ" :author "Ellen G. White" :language "en" :published 1892}
+      {:rank 14 :title "Heidi's Years of Wandering and Learning" :author "Johanna Spyri" :language "de" :published 1880}
+      {:rank 15 :title "The Common Sense Book of Baby and Child Care" :author "Dr. Benjamin Spock" :language "en"
+       :published 1946}])
---
+++ test/solrclj/test_helpers.clj
@@ -0,0 +1,60 @@
+(ns solrclj.test-helpers
+  (:use [clojure.contrib.java-utils])
+  (:import [org.mortbay.jetty.webapp WebAppContext]
+	   [org.mortbay.jetty Connector Server Request Response Handler]
+	   [org.mortbay.jetty.nio SelectChannelConnector]
+	   [org.mortbay.jetty.handler AbstractHandler HandlerCollection]
+           [org.mortbay.jetty.bio SocketConnector]
+           [org.mortbay.jetty.security SslSocketConnector]
+	   [java.io File]))
+
+(def test-solr-root "test-solr")
+(def test-embedded-book-conf {:dir test-solr-root :core "books"})
+(def test-http-conf {:type :http :port 2345})
+
+(defn map-path
+  "Helper to map relative paths to absolute paths."
+  ([path] (.getAbsolutePath (File. path)))
+  ([root path] (.getAbsolutePath (File. root path))))
+
+(defn create-web-app-context [path descriptor war]
+  (doto (WebAppContext.)
+    (.setContextPath path)
+    (.setWar war)
+    (.setDefaultsDescriptor descriptor)))
+
+(defn create-connector [port]
+  (doto (SelectChannelConnector.)
+    (.setPort port)))
+
+(defn create-handler-collection [handlers]
+  (doto (HandlerCollection.)
+    (.setHandlers (into-array Handler handlers))))
+
+(defn create-solr-web-app [root]
+  (System/setProperty "solr.solr.home" "test-solr")
+  (create-web-app-context
+	  "/solr"
+	  (map-path root "etc/webdefault.xml")
+	  (map-path root "webapps/solr.war")))
+
+(defn start-jetty
+  []
+  (doto (Server.)
+    (.addConnector (create-connector 2345))
+    (.setHandler (create-solr-web-app "test-jetty"))
+    (.start)))
+       
+(defn stop-jetty [#^Server jetty-server]
+  (.stop jetty-server))
+
+(defmacro with-jetty-solr
+  [body]
+  `(let [j# (start-jetty)]
+     (~@body)
+     (stop-jetty j#)
+     (.join j#)))
+
+(defn delete-test-core
+  [core]
+  (delete-file-recursively (map-path test-solr-root core)))
